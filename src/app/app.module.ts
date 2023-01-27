60022a16002abdbccbd96c42a6f665d3413226f9
leangarassino <leandro.garassino@gmail>
2023-01-27 17:26:07 -0300

    Generals Settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './app/home/home.component';
+import { SharedModule } from './app/shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [
+    MatToolbarModule,
+    CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
+})
+export class SharedModule { }
